bbc6cad194c6b9d4ed1eac6b7102e6b44011b1a6
Andrey Denisenko <francescogacczi@gmail.com>
2019-04-15 00:50:12 +0300

    after_chrash_v1.5

---
+++ Assets/Scripts/bat_activator.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class bat_activator : MonoBehaviour
+{
+    public ParticleSystem bats;
+
+    private void Start()
+    {
+        bats.gameObject.SetActive(false);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.GetComponent<PlayerPlatformerController>())
+        {
+            bats.gameObject.SetActive(true);
+
+            if (!bats.isPlaying)
+            {
+                bats.Play();
+                StartCoroutine(stopParticle());
+            }
+        }
+    }
+
+    IEnumerator stopParticle()
+    {
+        yield return new WaitForSeconds(0.1f);
+        Destroy(gameObject);
+    }
+}
