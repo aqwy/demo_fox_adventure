50b88c4d152e91a637db41bd85fe438411486a45
Andrey Denisenko <francescogacczi@gmail.com>
2019-06-02 21:24:23 +0300

    after_chrash_v2.5

---
+++ Assets/Scripts/Timeline/loadingMainMenu.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class loadingMainMenu : MonoBehaviour
+{
+    private void OnEnable()
+    {
+        SceneManager.LoadScene("StartMenuVariant2", LoadSceneMode.Single);
+    }
+}
