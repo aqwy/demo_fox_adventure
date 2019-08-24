fae3a43222c51557dde3a437023e2bdbc34a5344
Andrey Denisenko <francescogacczi@gmail.com>
2019-08-25 02:04:07 +0300

    back_to_gamedev_0.1

---
+++ Assets/Scripts/Cutscene/Countdown_endgame.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class Countdown_endgame : MonoBehaviour
+{
+    [SerializeField] private TextMeshProUGUI _countdown;
+
+    private void OnEnable()
+    {
+        StartCoroutine(countdownTxt());
+    }
+
+    IEnumerator countdownTxt()
+    {
+        _countdown.text = "3";
+        yield return new WaitForSeconds(1f);
+        _countdown.text = "2";
+        yield return new WaitForSeconds(1f);
+        _countdown.text = "1";
+        yield return new WaitForSeconds(1f);
+        gameObject.SetActive(false);
+    }
+}
