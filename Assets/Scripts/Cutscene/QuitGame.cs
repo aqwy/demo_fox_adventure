70936527f25f06cf76ced6fefdde16dd0daa0f2f
Andrey Denisenko <francescogacczi@gmail.com>
2019-08-30 00:40:02 +0300

    back_to_gamedev_0.2

---
+++ Assets/Scripts/Cutscene/QuitGame.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using FoxAdventure2d;
+
+public class QuitGame : MonoBehaviour
+{
+    [SerializeField] private StartUI _stui;
+    private void OnEnable()
+    {      
+        _stui.Quit();
+        Debug.Log("quit");
+    }
+}
--- Assets/Scripts/bat_activator.cs
+++ Assets/Scripts/bat_activator.cs
@@ -5,28 +5,39 @@ using UnityEngine;
 public class bat_activator : MonoBehaviour
 {
     public ParticleSystem bats;
+    public AudioSource batsAudios;
+    public AudioClip batsSound;
+
+    private bool _soundPlayed;
 
     private void Start()
     {
         bats.gameObject.SetActive(false);
+        _soundPlayed = false;
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
-    {
+    {       
         if (collision.gameObject.GetComponent<PlayerPlatformerController>())
         {
             bats.gameObject.SetActive(true);
-
+            
             if (!bats.isPlaying)
-            {
+            {              
                 bats.Play();
-                StartCoroutine(stopParticle());
+                StartCoroutine(stopParticle());               
+            }
+
+            if (bats.isPlaying && !_soundPlayed)
+            {
+                batsAudios.PlayOneShot(batsSound);
+                _soundPlayed = true;
             }
         }
     }
 
     IEnumerator stopParticle()
-    {
+    {        
         yield return new WaitForSeconds(0.1f);
         Destroy(gameObject);
     }
